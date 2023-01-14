0316218c7548d9c29867406dfa51ace48b1ba9e7
Grégor JOUET <gregor.jouet@laposte.net>
2023-01-14 11:50:48 +0100

    ok

---
+++ robot_management/redis_connector/redis_communicator.py
@@ -0,0 +1,20 @@
+import redis
+
+from typing import Any
+from robot_management.redis_connector.redis_communicator import RobotRedisElement
+from robot_management.utils import singleton
+
+
+@singleton
+class RedisConnector():
+
+    def __init__(self) -> None:
+        self.redis = redis.Redis(host="127.0.0.1", port = 6379)
+
+    def get(self, element: RobotRedisElement) -> Any:
+        return self.redis.get(element.value)
+
+    def set(self, element: RobotRedisElement, value: Any) -> None:
+        self.redis.set(element.value, value)
+
+        
